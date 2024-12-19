b1ea5b85cf161eaf2f03589d22980a29c268fca1
Ellie <elham.azizi749@gmail.com>
2024-12-19 16:20:47 -0500

    Finalized encoder of the model.
    Rich embeddings can be retrieved via encoder (no training though).
    Test sample provided.

--- src/network/network_architecture.py
+++ src/network/network_architecture.py
@@ -17,13 +17,13 @@ class PositionalEncoding():
         self.embedding_dim = (embedding_dim // 2) - 1
         self.embedding_matrix = np.zeros((sequence_length, embedding_dim))
         
-    def __call__(self):
+    def __call__(self, ):
         for pos in range(self.sequence_length):
             for i in range(self.embedding_dim):
                 self.embedding_matrix[pos, 2*i] = math.sin(pos / (math.pow(10000, (2*i / self.embedding_dim))))
                 self.embedding_matrix[pos, 2*i+1] = math.cos(pos / (math.pow(10000, (2*i / self.embedding_dim))))
 
-        return tf.convert_to_tensor(self.embedding_matrix)
+        return tf.convert_to_tensor(self.embedding_matrix, dtype=tf.float32)
     
 
 class ScaledMultiHeadAttention():
@@ -46,34 +46,45 @@ class ScaledMultiHeadAttention():
 
         for i in range(self.n_heads):
             weights = mlp_layer() # every time different weights are created
-            temp = (self.activation(weights[0](Q) @ tf.transpose(weights[1](K))/self.scale) ) @ weights[2](V)
+            temp = (self.activation(weights[0](Q) @ tf.transpose(weights[1](K), perm=(0, 2, 1))/self.scale) ) @ weights[2](V)
             if logit_matrix is None:
                 logit_matrix = temp
             else:
-                logit_matrix = tf.concat([logit_matrix, temp], axis = 1)
+                logit_matrix = tf.concat([logit_matrix, temp], axis = 2) # (batchsize, seq_length, embedding_dim  -> comcatenation should be applied on embedding_dim axis
 
-        #print(logit_matrix)
         return self.att_weight(logit_matrix)
 
 
+class FFN(Layer):
+    def __init__(self, embedding_dim):
+        super(FFN, self).__init__()
+        self.embedding_dim = embedding_dim
+
+    def call(self, x): #dense is applied to the last dimension automatically
+        return Dense(self.embedding_dim)(Dense(4*self.embedding_dim, activation='relu')(x)) #according to the paper
+
 
 class Encoder(Layer):
-    def __init__(self, vocab_size, embedding_dim):
+    def __init__(self, vocab_size, sequence_length, embedding_dim):
         super(Encoder, self).__init__()
         self.e_input = Embedding(vocab_size, embedding_dim, mask_zero=True)
-        self.positional_encoding = PositionalEncoding(20, 128)
+        self.positional_encoding = PositionalEncoding(sequence_length, embedding_dim)
         # self.input_matrix = self.e_input + self.positional_encoding
-        self.mha = ScaledMultiHeadAttention()
+        self.mha = ScaledMultiHeadAttention(4, embedding_dim)
         self.norm = LayerNormalization()
+        self.ffn = FFN(embedding_dim)
 
 
     def call(self, input_layer):
         x = self.e_input(input_layer)
-        x = self.positional_encoding(x)
-        x = self.e_input + x
+        print(x)
+        pe = self.positional_encoding()
+        x = pe + x
         y = self.mha(x, x, x)
-        x = self.norm(y + x)
-
+        z = self.norm(y + x)
+        x = self.ffn(z)
+        x = self.norm(x + z)
+        return x
 
 
 
@@ -93,3 +104,9 @@ Q = tf.convert_to_tensor([[2,3], [4,2]])
 mha = ScaledMultiHeadAttention(4, 4)
 print(mha(Q, Q, Q))
 '''
+
+data = np.array([[2, 3, 4, 5, 0], [6, 7, 8, 9, 0]])
+
+enc = Encoder(10, 5, 4)
+inp = Input(shape=(None,))
+print(enc(data))
